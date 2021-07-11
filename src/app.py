3b4c0649ed542e58802efebceb26d45f9d4364b1
Amira <mehriz.amira@gmail.com>
2021-07-11 03:24:27 +0200

    app server

---
+++ src/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, redirect, render_template, request, session
+#for cookies setting
+
+name="app"
+# Configure application
+app = Flask(name)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=['GET'])
+def index():
+  return render_template("main.html")
+
+
+@app.route("/uploaded", methods=["POST"])
+def upload_file():
+    uploaded_image= request.files['image']
+    if uploaded_image.filename != '':
+        uploaded_image.save("input/" + uploaded_image.filename)
+
+    uploaded_video = request.files['video']
+    if uploaded_video.filename != '':
+        uploaded_video.save("input/" + uploaded_video.filename)
+    return render_template("output.html",url="static/output/" + uploaded_video.filename)
