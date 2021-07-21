725d81ab651effd27467c10665de97555ec25124
Amira <mehriz.amira@gmail.com>
2021-07-21 02:17:20 +0200

    added rating

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, render_template, request, session
 from demo import main
-
+from flask_sqlalchemy import SQLAlchemy
 
 name="app"
 # Configure application
@@ -8,6 +8,21 @@ app = Flask(name)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# SQL alchemy configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class Rating(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, unique=False, nullable=False)
+
+    def __repr__(self):
+        return str(self.rating)
+
+db.create_all()
+
+
 
 # Ensure responses aren't cached
 @app.after_request
@@ -49,7 +64,7 @@ def upload_file():
         vidpath = vidpath +"1.mp4"
     elif option == "vid2":
         vidpath = vidpath +"3.mp4"
-    elif option == "vid4":
+    elif option == "vid3":
         vidpath = vidpath +"4.mp4"
 
     uploaded_video = request.files['video']
@@ -62,3 +77,27 @@ def upload_file():
     result_video = 'static/output/output.mp4')
 
     return render_template("output.html",originalURL = vidpath, outputURL="static/output/output.mp4" )
+
+
+
+@app.route("/ThankYou", methods=["POST"])
+def take_rating():
+    rating_score = request.form.get('rate_options')
+    if rating_score == "rate1" :
+        score=1
+    elif rating_score == "rate2":
+        score=2
+    elif rating_score == "rate3":
+        score=3
+    elif rating_score == "rate4":
+        score=4
+    elif rating_score == "rate5":
+        score=5
+
+    rating_ = Rating(rating = score)
+    db.session.add(rating_)
+
+    db.session.commit()
+
+    current_rating_ = Rating.query.all() # hhhh sorry
+    return render_template("thankyou.html", current_rating = current_rating_)
